fc7b4af038ebde13f35edd645784330405b20631
bcgocer <bcgocer@yukatech.com>
2024-02-05 11:34:33 +0300

    feat: added pub

--- cmd/sub/main.go
+++ cmd/sub/main.go
@@ -1,1 +1,145 @@
-package sub
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/gofiber/fiber/v3"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/bulutcan99/kafka_pubsub/models"
+
+	"github.com/IBM/sarama"
+)
+
+const (
+	ConsumerGroup      = "notifications-group"
+	ConsumerTopic      = "notifications"
+	ConsumerPort       = ":3030"
+	KafkaServerAddress = "localhost:9094"
+)
+
+type UserNotifications map[int][]models.Notification
+
+type NotificationStore struct {
+	data UserNotifications
+	mu   sync.RWMutex
+}
+
+func (ns *NotificationStore) Add(userID int, notification models.Notification) {
+	ns.mu.Lock()
+	defer ns.mu.Unlock()
+	ns.data[userID] = append(ns.data[userID], notification)
+}
+
+func (ns *NotificationStore) Get(userID int) []models.Notification {
+	ns.mu.RLock()
+	defer ns.mu.RUnlock()
+	return ns.data[userID]
+}
+
+type Consumer struct {
+	store *NotificationStore
+}
+
+func (*Consumer) Setup(sarama.ConsumerGroupSession) error   { return nil }
+func (*Consumer) Cleanup(sarama.ConsumerGroupSession) error { return nil }
+
+func (consumer *Consumer) ConsumeClaim(groupSession sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for msg := range claim.Messages() {
+		var notification models.Notification
+		err := json.Unmarshal(msg.Value, &notification)
+		if err != nil {
+			log.Printf("failed to unmarshal notification: %v", err)
+			continue
+		}
+		userId := notification.To.ID
+		fmt.Println("CONSUMER: ", userId, notification)
+		consumer.store.Add(userId, notification)
+		fmt.Println("CONSUMER: ", consumer.store.Get(userId))
+		groupSession.MarkMessage(msg, "")
+	}
+	return nil
+}
+
+func initializeConsumerGroup() (sarama.ConsumerGroup, error) {
+	config := sarama.NewConfig()
+
+	consumerGroup, err := sarama.NewConsumerGroup(
+		[]string{KafkaServerAddress}, ConsumerGroup, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize consumer group: %w", err)
+	}
+
+	return consumerGroup, nil
+}
+
+func setupConsumerGroup(ctx context.Context, store *NotificationStore) {
+	consumerGroup, err := initializeConsumerGroup()
+	if err != nil {
+		log.Printf("initialization error: %v", err)
+	}
+	defer consumerGroup.Close()
+
+	consumer := &Consumer{
+		store: store,
+	}
+
+	for {
+		err = consumerGroup.Consume(ctx, []string{ConsumerTopic}, consumer)
+		if err != nil {
+			log.Printf("error from consumer: %v", err)
+		}
+		if ctx.Err() != nil {
+			return
+		}
+	}
+}
+
+func handleNotifications(ctx fiber.Ctx, store *NotificationStore) error {
+	userID, err := ctx.ParamsInt("user_id")
+	if err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   "user id must be a number",
+		})
+	}
+
+	notes := store.Get(userID)
+	if len(notes) == 0 {
+		return ctx.Status(http.StatusNoContent).JSON(fiber.Map{
+			"error": true,
+			"msg":   "no notifications found",
+		})
+	}
+
+	return nil
+}
+
+func main() {
+	store := &NotificationStore{
+		data: make(UserNotifications),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go setupConsumerGroup(ctx, store)
+	defer cancel()
+
+	app := fiber.New()
+	app.Get("/:user_id/receive", func(ctx fiber.Ctx) error {
+		err := handleNotifications(ctx, store)
+		if err != nil {
+			return err
+		}
+		return ctx.Status(http.StatusOK).JSON(fiber.Map{
+			"error": false,
+			"msg":   "notifications found",
+			"data":  store.Get(2),
+		})
+	})
+
+	fmt.Printf("Kafka Consumer group %s started at http://localhost:%s\n", ConsumerGroup, ConsumerPort)
+	log.Fatal(app.Listen(":3030"))
+}
---
+++ go.mod
@@ -0,0 +1,35 @@
+module github.com/bulutcan99/kafka_pubsub
+
+go 1.21.6
+
+require (
+	github.com/IBM/sarama v1.42.1 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.4.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/gofiber/fiber/v3 v3.0.0-20240201123843-a348c1dd2c9b // indirect
+	github.com/gofiber/utils/v2 v2.0.0-beta.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pierrec/lz4/v4 v4.1.18 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
